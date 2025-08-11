220c0645b9e54e69a8aeb3b2c9da80753cce4aa8
Alex Casalboni <alex@edgee.cloud>
2025-08-11 10:23:00 +0200

    update helpers to latest

--- src/helpers/body.rs
+++ src/helpers/body.rs
@@ -3,45 +3,49 @@ use anyhow::Result;
 use bytes::Bytes;
 
 pub trait FromBody: Sized {
-    fn from_body(body: IncomingBody) -> Result<Self>;
-}
+    fn from_data(data: Bytes) -> Result<Self>;
 
-impl FromBody for () {
-    fn from_body(_: IncomingBody) -> Result<Self> {
-        Ok(())
+    fn from_body(body: IncomingBody) -> Result<Self> {
+        Self::from_data(body.read()?)
     }
 }
 
+pub trait IntoBody: Sized {
+    fn into_body(self) -> Result<Bytes>;
+
+    #[allow(unused_variables)]
+    fn extend_response_parts(&self, parts: &mut http::response::Parts) {}
+}
+
 impl FromBody for IncomingBody {
+    fn from_data(_: Bytes) -> Result<Self> {
+        unimplemented!("Should never be called")
+    }
+
     fn from_body(body: IncomingBody) -> Result<Self> {
         Ok(body)
     }
 }
 
 impl FromBody for Bytes {
-    fn from_body(body: IncomingBody) -> Result<Self> {
-        body.read()
+    fn from_data(data: Bytes) -> Result<Self> {
+        Ok(data)
     }
-}
-
-pub trait IntoBody: Sized {
-    fn into_body(self) -> Result<Bytes>;
-    fn handle_error(err: anyhow::Error) -> http::Response<Bytes>;
-
-    #[allow(unused_variables)]
-    fn extend_response_parts(&self, parts: &mut http::response::Parts) {}
 }
 
 impl IntoBody for Bytes {
     fn into_body(self) -> Result<Bytes> {
         Ok(self)
     }
+}
 
-    fn handle_error(_: anyhow::Error) -> http::Response<Bytes> {
-        http::Response::builder()
-            .status(http::StatusCode::INTERNAL_SERVER_ERROR)
-            .body(Bytes::new())
-            .unwrap()
+impl FromBody for () {
+    fn from_data(_: Bytes) -> Result<Self> {
+        Ok(())
+    }
+
+    fn from_body(_: IncomingBody) -> Result<Self> {
+        Ok(())
     }
 }
 
@@ -49,9 +53,11 @@ impl IntoBody for () {
     fn into_body(self) -> Result<Bytes> {
         Ok(Bytes::new())
     }
+}
 
-    fn handle_error(err: anyhow::Error) -> http::Response<Bytes> {
-        Bytes::handle_error(err)
+impl FromBody for String {
+    fn from_data(data: Bytes) -> Result<Self> {
+        String::from_utf8(data.into()).map_err(Into::into)
     }
 }
 
@@ -59,20 +65,40 @@ impl IntoBody for String {
     fn into_body(self) -> Result<Bytes> {
         Ok(Bytes::from(self))
     }
+}
 
-    fn handle_error(err: anyhow::Error) -> http::Response<Bytes> {
-        Bytes::handle_error(err)
+impl<T: FromBody> FromBody for Option<T> {
+    fn from_data(data: Bytes) -> Result<Self> {
+        if data.is_empty() {
+            Ok(None)
+        } else {
+            Ok(Some(T::from_data(data)?))
+        }
+    }
+}
+
+impl<T: IntoBody> IntoBody for Option<T> {
+    fn into_body(self) -> Result<Bytes> {
+        match self {
+            Some(value) => value.into_body(),
+            None => Ok(Bytes::new()),
+        }
+    }
+
+    fn extend_response_parts(&self, parts: &mut http::response::Parts) {
+        if let Some(value) = self {
+            value.extend_response_parts(parts);
+        }
     }
 }
 
 // Data types
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Json<T>(pub T);
 
 impl<T: serde::de::DeserializeOwned> FromBody for Json<T> {
-    fn from_body(body: IncomingBody) -> Result<Self> {
-        let bytes = body.read()?;
+    fn from_data(bytes: Bytes) -> Result<Self> {
         let data = serde_json::from_slice(&bytes)?;
         Ok(Self(data))
     }
@@ -87,10 +113,6 @@ impl<T: serde::Serialize> IntoBody for Json<T> {
         Ok(buf.into_inner().freeze())
     }
 
-    fn handle_error(err: anyhow::Error) -> http::Response<Bytes> {
-        json_error_response(err.to_string())
-    }
-
     fn extend_response_parts(&self, parts: &mut http::response::Parts) {
         parts
             .headers
@@ -99,15 +121,25 @@ impl<T: serde::Serialize> IntoBody for Json<T> {
     }
 }
 
+#[derive(Debug, Clone)]
+pub struct RawJson<T>(pub T);
+
+impl<T: Into<Bytes>> IntoBody for RawJson<T> {
+    fn into_body(self) -> Result<Bytes> {
+        Ok(self.0.into())
+    }
+
+    fn extend_response_parts(&self, parts: &mut http::response::Parts) {
+        Json(()).extend_response_parts(parts)
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct Html<T>(pub T);
 
 impl<T: Into<Bytes>> IntoBody for Html<T> {
     fn into_body(self) -> Result<Bytes> {
         Ok(self.0.into())
-    }
-
-    fn handle_error(_err: anyhow::Error) -> http::Response<Bytes> {
-        html_error_response()
     }
 
     fn extend_response_parts(&self, parts: &mut http::response::Parts) {
@@ -116,29 +148,4 @@ impl<T: Into<Bytes>> IntoBody for Html<T> {
             .entry(http::header::CONTENT_TYPE)
             .or_insert(http::HeaderValue::from_static("text/html; charset=utf-8"));
     }
-}
-
-// Error responses
-
-fn html_error_response() -> http::Response<Bytes> {
-    http::Response::builder()
-        .status(http::StatusCode::INTERNAL_SERVER_ERROR)
-        .header(http::header::CONTENT_TYPE, "text/html")
-        .body(super::ERROR_PAGE)
-        .unwrap()
-}
-
-fn json_error_response(msg: impl Into<String>) -> http::Response<Bytes> {
-    http::Response::builder()
-        .status(http::StatusCode::INTERNAL_SERVER_ERROR)
-        .header(http::header::CONTENT_TYPE, "application/json")
-        .body(
-            serde_json::to_vec(&serde_json::json!({
-                "error": "Internal server error",
-                "message": msg.into(),
-            }))
-            .unwrap()
-            .into(),
-        )
-        .unwrap()
 }
